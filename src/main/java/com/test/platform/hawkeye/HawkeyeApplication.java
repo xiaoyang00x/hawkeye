ab72211b6ddfde619c0d3a2119a0a460318a04ea
yangyu09 <yangyu09@meituan.com>
2019-07-26 15:20:25 +0800

    init

---
+++ src/main/java/com/test/platform/hawkeye/HawkeyeApplication.java
@@ -0,0 +1,13 @@
+package com.test.platform.hawkeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkeyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawkeyeApplication.class, args);
+	}
+
+}
