add616cd7b299451c22260262c7e68d2ee810e1d
yangyu09 <yangyu09@meituan.com>
2019-08-28 11:40:26 +0800

    init

---
+++ src/main/java/com/test/platform/hawkeye/scheduled/test.java
@@ -0,0 +1,7 @@
+package com.test.platform.hawkeye.scheduled;
+
+/**
+ * Created by yangyu on 2019/8/28.
+ */
+public class test {
+}
---
+++ src/main/java/com/test/platform/hawkeye/service/test.java
@@ -0,0 +1,7 @@
+package com.test.platform.hawkeye.service;
+
+/**
+ * Created by yangyu on 2019/8/28.
+ */
+public class test {
+}
